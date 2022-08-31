10ec10fac4bd126437041b4b1c1ece443f7b8c04
George Floyd <39600182+tzwel@users.noreply.github.com>
2022-08-31 00:42:39 +0000

    Create ripple.js

---
+++ ripple.js
@@ -0,0 +1,41 @@
+function ripple(settings) {
+
+	if (!settings.node.innerHTML.includes('<ripple')) {
+		const rippleColor = window.getComputedStyle(settings.node).getPropertyValue("color");
+
+		const dimensions = {
+			x: settings.node.clientWidth,
+			y: settings.node.clientHeight
+		}
+
+		const ripple = document.createElement('ripple');
+		settings.node.appendChild(ripple);
+
+		const rippleWidth = Math.max(dimensions.x, dimensions.y)
+
+		ripple.style.transform = `scale(${settings.size})`;
+		ripple.style.top = window.event.offsetY - rippleWidth / 2 + 'px';
+		ripple.style.left = window.event.offsetX - rippleWidth / 2 + 'px';
+		ripple.style.width = rippleWidth + 'px'
+		ripple.style.height = rippleWidth + 'px'
+		ripple.style.background = rippleColor
+
+		setTimeout(function(){
+			ripple.style.transition = `top ${settings.speed}s, left ${settings.speed}s, width ${settings.speed}s, height ${settings.speed}s, transform ${.4}s, opacity ${settings.speed}s`
+
+			ripple.style.top = dimensions.y/2 - rippleWidth/2 + 'px'
+			ripple.style.left = dimensions.x/2 - rippleWidth/2 + 'px'
+			ripple.style.transform = "scale(1.1)";
+		}, 0);
+
+		document.addEventListener('mouseup', ()=> {
+			ripple.classList.add('ripple-closing');
+			window.setTimeout(deleteRipple, settings.speed * 1000);
+			function deleteRipple() {
+				ripple.remove();
+			}
+		})
+	
+	}
+
+}
